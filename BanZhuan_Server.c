18298525e1fdaaa0638c6fc0ebdc20a34cbc8076
zhz <zhz@meitu.com>
2017-08-22 09:46:42 +0800

    保存

--- BanZhuan_Server.c
+++ BanZhuan_Server.c
@@ -1,28 +1,48 @@
 #include "BanZhuan_Server.h"
 
 
+static void
+Banzhuan_Reconstruct_File(char *start, size_t size)
+{
+
+}
+
+static void
+Start_worker_thread_pool()
+{
+    thread_init()
+}
+
 int main(void)
 {
     int fd, ret;
-    char *start, *buf, *p;
+    char *start,  *p;
     struct stat file_stat;
 
     fd = open("/tmp/robintest.txt", 0);
     //fd = open("/tmp/tunnelblick-downscript-needs-to-be-run.txt", O_CREAT, O_RDWR);
 
     fstat(fd, &file_stat);
-    buf = malloc(file_stat.st_size);
+    globle_cache = malloc(file_stat.st_size);
 
     //printf("len = %d\n", (int)file_stat.st_size);
 
     start = mmap(NULL, file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     ret = 0;
-    p = buf;
+    p = globle_cache;
+
+    Start_comm();
+
+    Start_read_thread();
+    Start_worker_thread_pool();
+    Banzhuan_Reconstruct_File(start, file_stat.st_size);
+    /*
     do {
        *p = start[ret++]; 
        p++;
     } while(ret < file_stat.st_size);
-
+    */
+    memcpy(globle_cache, start, file_stat.st_size);
     //printf("%s\n", start);
     //start_workers();
 
--- BanZhuan_Server.h
+++ BanZhuan_Server.h
@@ -10,4 +10,27 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define THREAD_POLL_NUM 32
+
+typedef struct bz_thread_s bz_thread_t;
+typedef struct bz_comm_s bz_comm_t;
+
+static void Banzhuan_Reconstruct_File(char *start, size_t size);
+static void Start_worker_thread(); 
+
+char *globle_cache;
+
+struct bz_thread_s{
+   bz_comm_t *comm; 
+};
+
+struct bz_comm_s{
+    int listen_fd;
+
+};
+
+bz_comm_t *comm;
+bz_thread_t threads_p[THREAD_POLL_NUM];
+
+
 #endif
